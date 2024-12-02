1ec5d5e1cabd5e3b56fc7e83480052647ab9e487
Roosun <roosa.sundstrom@hotmail.fi>
2024-12-02 14:05:00 +0200

    Sign-out function

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from './assets/config/firebaseConfig';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import LoginScreen from './assets/components/LoginScreen';
 import styles from './assets/styles/styles';
 import TodoListContainer from './assets/components/TodoListContainer';
@@ -22,6 +22,18 @@ export default function App() {
     return unsubscribe;
   }, []);
 
+  // Uloskirjautumistoiminto
+  const handleSignOut = async (navigation) => {
+    try {
+      await signOut(auth);
+      console.log("Käyttäjä uloskirjautui");
+      setUser(null); // Käyttäjä kirjautuu ulos
+      navigation.replace("Login"); // Siirrä käyttäjä Login-näkymään
+    } catch (error) {
+      console.error("Uloskirjautumisessa tapahtui virhe:", error);
+    }
+  };
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -29,14 +41,26 @@ export default function App() {
       </View>
     );
   }
-  
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={user ? 'TodoList' : 'Login'}>
         
         <Stack.Screen name="Login" component={LoginScreen} />
         
-        <Stack.Screen name="TodoList" component={TodoListContainer} />
+        <Stack.Screen 
+          name="TodoList" 
+          component={TodoListContainer} 
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Button
+                onPress={() => handleSignOut(navigation)} // Siirry navigaatiolla ulos
+                title="Ulos"
+                color="#ff6347"
+              />
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
